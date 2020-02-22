08660dc4840051b1541961cd6f85691738dcb032
Nileshlad <ladnilesh1111@gmail.com>
2020-02-22 16:04:43 +0530

    ADDED:Toreport the number of times the dice was played to win the game and also the possition after every die role

--- Sanke_And_Ladder_Game.sh
+++ Sanke_And_Ladder_Game.sh
@@ -9,34 +9,44 @@ INITIAL_POSITION=0
 
 #Variable
 playerPosition=0
+diceCounter=0
 valid=true
 
-function checkForOption(){
-	while [ $valid ]
-	do
-		 die=$((RANDOM%6+1))
-		 option=$((RANDOM%3))
-			if [[ $option -eq $LADDER ]]
-			then
-				playerPosition=$(( $playerPosition + $die ))
-			elif [[ $option -eq $SNAKE ]]
-			then
-				playerPosition=$(( $playerPosition - $die ))
-			if [[ $playerPosition -eq $INITIAL_POSITION ]]
-			then
-				playerPosition=$(( $playerPosition ))
-			fi
-			fi
+#TO DECLARE FUNCTION
+declare -A diceCount
 
-			if [[ $playerPosition -lt $WINNING_POSITION ]]
-			then
-				playerPosition=$(( $playerPosition ))
-			elif [[ $playerPosition -eq $WINNING_POSITION ]]
-			then
-				echo "You won"
-				break
-			fi
-	done
+function checkForOption(){
+while [ $valid ]
+	do
+		#TO VARIABLE ROLL DIE AND OPTION
+		rollDie=$((RANDOM%6+1))
+		option=$((RANDOM%3))
+	if [[ $option -eq $LADDER ]]
+	then
+		playerPosition=$(( $playerPosition + $rollDie ))
+		diceCounter=$(($diceCounter+1))
+	elif [[ $option -eq $SNAKE ]]
+	then
+		playerPosition=$(( $playerPosition - $rollDie ))
+	
+	if [[ $playerPosition -eq $INITIAL_POSITION ]]
+	then
+		playerPosition=$(( $INITIAL_POSITION ))
+	fi
+	fi
+
+	if [[ $playerPosition -lt $WINNING_POSITION ]]
+	then
+		playerPosition=$(( $playerPosition ))
+	elif [[ $playerPosition -eq $WINNING_POSITION ]]
+	then
+		echo "You won"
+		break
+	fi
+done
+#TO PEINT 
+echo "Winning position " $playerPosition "die roll " $diceCounter
 }
 
-	checkForOption
+#TO FUNCTION CALL
+checkForOption
