139a7cf5ba82ff13314958b734e0aba7a1db5ffa
Nileshlad <ladnilesh1111@gmail.com>
2020-02-22 15:32:44 +0530

    ADDED:To player gets to exact winning position

--- Sanke_And_Ladder_Game.sh
+++ Sanke_And_Ladder_Game.sh
@@ -1,38 +1,42 @@
 #!/bin/bash -x
 echo "---------------------------------------------------welcome Snake and ladder game---------------------------------------------------"
 
-#Constant for the program
+#CONSTANT
 LADDER=1
 SNAKE=2
-NO_PLAY=0
 WINNING_POSITION=100
 INITIAL_POSITION=0
 
+#Variable
 playerPosition=0
 valid=true
 
 function checkForOption(){
 	while [ $valid ]
 	do
-		rollDie=$((RANDOM%6+1))
-		option=$((RANDOM%3))
-		if [ $option -eq $LADDER ]
-		then
-			playerPosition=$(( $playerPosition + $rollDie ))
-		elif [ $option -eq $SNAKE ]
-		then
-			playerPosition=$(( $playerPosition - $rollDie ))
-		else
-			echo "no play"
-		fi
+		 die=$((RANDOM%6+1))
+		 option=$((RANDOM%3))
+			if [[ $option -eq $LADDER ]]
+			then
+				playerPosition=$(( $playerPosition + $die ))
+			elif [[ $option -eq $SNAKE ]]
+			then
+				playerPosition=$(( $playerPosition - $die ))
+			if [[ $playerPosition -eq $INITIAL_POSITION ]]
+			then
+				playerPosition=$(( $playerPosition ))
+			fi
+			fi
 
-		if [ $playerPosition -gt $WINNING_POSITION ]
-		then
-			playerPosition=$(( $INITIAL_POSITION ))
-		elif [ $playerPosition -eq $WINNING_POSITION ]
-		then	
-		break
-		fi
+			if [[ $playerPosition -lt $WINNING_POSITION ]]
+			then
+				playerPosition=$(( $playerPosition ))
+			elif [[ $playerPosition -eq $WINNING_POSITION ]]
+			then
+				echo "You won"
+				break
+			fi
 	done
 }
-checkForOption
+
+	checkForOption
